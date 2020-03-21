d9f0ccf5db1b2cb38f534db761be775bc396f722
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-03-20 17:47:08 -0700

    attempting to render js file on heroku

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, "public"));
 //Setting up handlebars
 app.engine(
     "handlebars",
