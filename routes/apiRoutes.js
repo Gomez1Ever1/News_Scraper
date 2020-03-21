2a31304bd6d7fc18f5e799d97e34cbac7fd00258
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-03-20 18:59:40 -0700

    resolving route issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,24 @@ const db = require("../models");
 module.exports = function (app) {
     app.post("/article/:id", function (req, res) {
         console.log(req.body)
-        db.Article.findOne({ _id: req.body.savedArticle.thisId }, function (err, result) {
-            console.log(result)
-            const clonedResult = {
-                headline: result.headline,
-                link: result.link,
-                art_img: result.art_img,
-                summary: result.summary
-            };
+        db.Article.findOne({ _id: req.body.savedArticle.thisId })
+            .then(function (result) {
+                console.log(result)
+                const clonedResult = {
+                    headline: result.headline,
+                    link: result.link,
+                    art_img: result.art_img,
+                    summary: result.summary
+                }
+                db.savedArticle.create(clonedResult)
+                    .then(function (newArticle) {
+                        res.json(newArticle);
+                        console.log(newArticle)
+                        console.log("Article Saved")
+                    });
+            })
+    });
 
-            db.savedArticle.create(clonedResult)
-                .then(function (newArticle) {
-                    res.json(newArticle);
-                    console.log(newArticle)
-                    console.log("Article Saved")
-                });
-        })
-    })
     app.delete("/deleteArticles", function (req, res) {
         db.Article.deleteMany({})
             .then(function (res) {
